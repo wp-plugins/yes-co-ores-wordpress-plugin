b23540d85499a9a866b48e937c141261040ae4ea
kees-brandenburg <kees-brandenburg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-03 13:58:50 +0000

    Fixed: plugin version / wp version check (for remote debugging)
    git-svn-id: https://plugins.svn.wordpress.org/yes-co-ores-wordpress-plugin/trunk@942876 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/classes/yog_checks.php
+++ includes/classes/yog_checks.php
@@ -3,102 +3,106 @@ class YogChecks
 {
 	/**
 	 * Check for errors
-	 * 
+	 *
 	 * @param void
 	 * @return array
 	 */
 	public static function checkForErrors()
 	{
 		$errors   = array();
-		
+
 		// Upload folder writable
 		$uploadDir = wp_upload_dir();
-		
+
 		if (!empty($uploadDir['error']))
 			$errors[] = $uploadDir['error'];
 		else if (!is_writeable($uploadDir['basedir']))
 			$errors[] = 'De upload map van uw WordPress installatie is beveiligd tegen schrijven. Dat betekent dat er geen afbeelingen van de objecten gesynchroniseerd kunnen worden. Stel onderstaande locatie zo in, dat deze beschreven kan worden door de webserver. <br /><i><b>' . $uploadDir['basedir'] .'</b></i>';
-		
+
 		// PHP version check
 		if (!version_compare(PHP_VERSION, '5.2.1', '>='))
 			$errors[] = 'PHP versie ' . PHP_VERSION . ' is gedetecteerd, de plugin vereist minimaal PHP versie 5.2.1. Neem contact op met je hosting provider om de PHP versie te laten upgraden';
-		
+
 		// Lib XML check
 		if (!extension_loaded('libxml'))
 			$errors[] = 'De php librairy <b>libxml</b> is niet geinstalleerd. Neem contact op met je hosting provider om libxml te laten installeren';
-		
+
 		// allow_url_fopen / CURL check
 		if (!ini_get('allow_url_fopen') && !function_exists('curl_init'))
 			$errors[] = 'De php setting <b>allow_url_fopen</b> staat uit en de php librairy <b>CURL</b> is niet geinstalleerd. Voor de synchronisatie is 1 van deze 2 noodzakelijk. Neem contact op met je hosting provider hierover.';
-	
+
 		// Wordpress version
 		global $wp_version;
 		if ((float) $wp_version < 3.0)
 			$errors[] = 'Wordpress versie ' . $wp_version . ' is gedetecteerd, voor deze plugin is Wordpress versie 3.0 of hoger vereist. Upgrade wordpress naar een nieuwere versie';
-		
+
 		return $errors;
 	}
 
 	/**
 	 * Check for warnings
-	 * 
+	 *
 	 * @param void
 	 * @return array
 	 */
 	public static function checkForWarnings()
 	{
 		$warnings = array();
-		
+
 		// Single huis template check
 		if (!is_file(get_template_directory() .'/single-huis.php'))
 			$warnings[] = 'Het ingestelde thema heeft op dit moment geen \'single-huis.php\' template. Er zal een alternatieve methode gebruikt worden voor het tonen van de Wonen object details.';
-		
+
 		// Single bedrijf template check
 		if (!is_file(get_template_directory() .'/single-bedrijf.php'))
 			$warnings[] = 'Het ingestelde thema heeft op dit moment geen \'single-bedrijf.php\' template. Er zal een alternatieve methode gebruikt worden voor het tonen van de BOG object details.';
-		
+
 		// Single NBpr template check
 		if (!is_file(get_template_directory() .'/single-yog-nbpr.php'))
 			$warnings[] = 'Het ingestelde thema heeft op dit moment geen \'single-yog-nbpr.php\' template. Er zal een alternatieve methode gebruikt worden voor het tonen van de Nieuwbouw Project details.';
-		
+
 		// Single NBpr template check
 		if (!is_file(get_template_directory() .'/single-yog-nbty.php'))
 			$warnings[] = 'Het ingestelde thema heeft op dit moment geen \'single-yog-nbty.php\' template. Er zal een alternatieve methode gebruikt worden voor het tonen van de Nieuwbouw type details.';
-		
+
 		// Wordpress version
 		global $wp_version;
 		if ((float) $wp_version >= 3.0 && (float) $wp_version < 3.1)
 			$warnings[] = 'Wordpress versie ' . $wp_version . ' is gedetecteerd, voor deze plugin raden we minimaal Wordpress versie 3.1 aan.';
-		
+
 		return $warnings;
 	}
-	
+
 	/**
 	 * Get wordpress settings
-	 * 
+	 *
 	 * @Param void
 	 * @return array
 	 */
 	public static function getSettings()
 	{
-		$settings = array();
-		
+    if (!function_exists('get_plugin_data'))
+      require_once(ABSPATH . 'wp-admin/includes/plugin.php' );
+
+		$settings   = array();
+
 		// Wordpress version
 		global $wp_version;
-		$settings['Wordpress version'] = (float) $wp_version;
-		
+		$settings['Wordpress version'] = $wp_version;
+
 		// Plugin version
-		$settings['Plugin version'] = YOG_PLUGIN_VERSION;
-		
+    $pluginData = get_plugin_data(dirname(__FILE__) . '/../../yesco-og.php');
+		$settings['Plugin version'] = $pluginData['Version'];
+
 		// PHP version
 		$settings['PHP version'] = PHP_VERSION;
-		
+
 		// allow_url_fopen
 		$settings['allow_url_fopen'] = (ini_get('allow_url_fopen')) ? 'enabled' : 'disabled';
-		
+
 		// CURL
-		$settings['CURL'] = function_exists('curl_init') ? 'enabled' : 'disabled'; 
-		
+		$settings['CURL'] = function_exists('curl_init') ? 'enabled' : 'disabled';
+
 		return $settings;
 	}
 }
