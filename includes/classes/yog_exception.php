c9a0cf93948b2e9a0198971862403754f9514cef
kees-brandenburg <kees-brandenburg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-06 09:13:19 +0000

    Fixed: typo
    git-svn-id: https://plugins.svn.wordpress.org/yes-co-ores-wordpress-plugin/trunk@1193151 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/classes/yog_exception.php
+++ includes/classes/yog_exception.php
@@ -7,7 +7,7 @@
   {
     const WRONG_REQUEST   = 1;
     const GLOBAL_ERROR    = 2;
-    const NO_SYSYEM_LINK  = 3;
+    const NO_SYSTEM_LINK  = 3;
 
     /**
     * @desc Determine human readable error
@@ -22,7 +22,7 @@
         case self::WRONG_REQUEST:
           return 'Signature does not match a local secret, did you use the wrong secret?';
           break;
-        case self::NO_SYSYEM_LINK:
+        case self::NO_SYSTEM_LINK:
           return 'Yes-co Open is not (yet) activated on this blog.';
           break;
         case self::GLOBAL_ERROR:
